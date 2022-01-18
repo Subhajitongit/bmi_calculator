8c88a354ffcbb27856e6fc323aea005e33e0df1b
Subhajit Gorai <subhajitthisside@gmail.com>
2022-01-18 21:58:21 +0530

    all done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, deprecated_member_use
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, deprecated_member_use, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +27,12 @@ class BmiCalculator extends StatefulWidget {
 
 class _BmiCalculatorState extends State<BmiCalculator> {
   int currentindex = 0;
+  String result = "";
+  double height = 0.0;
+  double weight = 0.0;
+
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +51,97 @@ class _BmiCalculatorState extends State<BmiCalculator> {
           child: Padding(
             padding: EdgeInsets.all(12.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   children: [
                     radioButton("Man", Colors.blue, 1),
                     radioButton("Women", Colors.pink, 0),
                   ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Your height in cms:",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  controller: heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "Enter Height",
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Your weight in kgs:",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  controller: weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: "Enter Weight",
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      height = double.parse(heightController.value.text);
+                      weight = double.parse(weightController.value.text);
+                      calcBMI(height, weight);
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.red)),
+                    child: Text("Calculate"),
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "$result",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                  ),
                 )
               ],
             ),
@@ -59,6 +149,14 @@ class _BmiCalculatorState extends State<BmiCalculator> {
         ),
       ),
     );
+  }
+
+  void calcBMI(double height, double weight) {
+    double res = weight / (height * height / 10000);
+    String bmi = res.toStringAsFixed(2);
+    setState(() {
+      result = bmi;
+    });
   }
 
   void changeIndex(int index) {
---
+++ lib/resultPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class resultPage extends StatelessWidget {
+  const resultPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.grey[200],
+      ),
+    );
+  }
+}
